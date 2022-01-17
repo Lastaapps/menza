ab921119515ee4d9255ae1b33350280930013536
Lasta apps <krasik.peta@seznam.cz>
2022-01-17 18:04:42 +0100

    buildSrc: KM Logging added v1.1.1

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -70,6 +70,7 @@ object Versions {
     const val KOTEST = "5.1.0"
     const val KOTLINX_DATETIME = "0.3.1"
     const val SQLDELIGHT = "1.5.3"
+    const val KM_LOGGING = "1.1.1"
     const val SKRAPE_IT = "1.1.7"
 
     //testing android
