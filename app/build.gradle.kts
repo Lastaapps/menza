2a2042369af8d7451f516f8851325f13bc159f50
Lasta apps <krasik.peta@seznam.cz>
2024-02-19 23:15:37 +0200

    build: Release 1.3.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "cz.lastaapps.menza"
 
         // have to be specified explicitly for FDroid to work
-        versionCode = 1030500 // 1x major . 2x minor . 2x path . 2x build diff
-        versionName = "1.3.5"
+        versionCode = 1030600 // 1x major . 2x minor . 2x path . 2x build diff
+        versionName = "1.3.6"
 
         minSdk = libs.versions.sdk.min.get().toInt()
         targetSdk = libs.versions.sdk.target.get().toInt()
